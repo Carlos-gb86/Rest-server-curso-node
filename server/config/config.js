0c7721f6c4b7a5f270a09ba680791f1be347cf50
Carlos <carlos.gil.berrocal@gmail.com>
2019-11-19 22:37:11 +0100

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 // ===========================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===========================================
+// ENTORNO
+// ===========================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ===========================================
+// BASE DE DATOS
+// ===========================================
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev'){
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+urlDB = 'mongodb+srv://berrocal:2PF&emKV@cluster0-lnir7.mongodb.net/cafe';
+// };
+process.env.URLDB = urlDB;
